385bb1e5f0034ffeddce304b45b43adc2cf66675
absinthriver <dalakovaak@gmail.com>
2023-06-06 11:49:17 +0200

    Added 2nd element to map.

--- src/main/java/samples/Sample.java
+++ src/main/java/samples/Sample.java
@@ -7,5 +7,6 @@ public class Sample {
     public static void main(String[] args) {
         Map<Integer, String> commits = new HashMap();
         commits.put(1, "1st commit");
+        commits.put(2, "2nd commit");
     }
 }
