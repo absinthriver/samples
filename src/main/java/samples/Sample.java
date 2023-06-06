661e076da5b96a33b01088694646e0a50eb1a1b3
absinthriver <dalakovaak@gmail.com>
2023-06-06 11:47:18 +0200

    Added 1st element to map.

--- src/main/java/samples/Sample.java
+++ src/main/java/samples/Sample.java
@@ -6,5 +6,6 @@ import java.util.Map;
 public class Sample {
     public static void main(String[] args) {
         Map<Integer, String> commits = new HashMap();
+        commits.put(1, "1st commit");
     }
 }
