452919bbdc3a14d71b68d675c6b92767e436896e
absinthriver <dalakovaak@gmail.com>
2023-06-06 11:45:51 +0200

    Sample class updated.

--- src/main/java/samples/Sample.java
+++ src/main/java/samples/Sample.java
@@ -1,4 +1,10 @@
 package main.java.samples;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sample {
+    public static void main(String[] args) {
+        Map<Integer, String> commits = new HashMap();
+    }
 }
