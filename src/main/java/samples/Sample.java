1b8980301d41445e9f0fe5b57b2ec1f81ef9eab4
absinthriver <dalakovaak@gmail.com>
2023-06-06 14:34:18 +0200

    Master 7

--- src/main/java/samples/Sample.java
+++ src/main/java/samples/Sample.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Sample {
     public static void main(String[] args) {
         Map<Integer, String> commits = new HashMap();
-        commits.put(1, "1st commit");
+        commits.put(1, "commit");
         commits.put(2, "2nd commit");
         commits.put(3, "3d commit");
         commits.put(4, "4th commit");
